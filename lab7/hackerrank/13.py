41468a8c1de7e97e3c69d7bd7a758defc03fd295
danagul11 <86469050+danagul11@users.noreply.github.com>
2022-04-01 12:31:03 +0600

    Add files via upload

---
+++ lab7/hackerrank/13.py
@@ -0,0 +1,10 @@
+#swap case
+def swap_case(s):
+    res = ''
+    res = ''.join([i.lower() if i.isupper() else i.upper() for i in s])
+    return res
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
